c15c3aa8b4d9064ccc031183442edd9f29dc9c44
hannanm335 <165081989+hannanm335@users.noreply.github.com>
2024-04-25 11:45:52 -0400

    Update group turtle project.py
    made a sunny and rainy environment

--- group turtle project.py
+++ group turtle project.py
@@ -1,7 +1,6 @@
 #import turtle library
 import turtle
-import math
-import random
+import time
 
 # function to set up turtle environment
 def environment():
@@ -10,9 +9,9 @@ def environment():
     screen.setup(width= 800, height = 800)
     screen.bgcolor('skyblue')
 
-def draw_background():
+def draw_sunny_background():
     pen = turtle.Turtle()
-    pen.speed(0) # set the drawing speed to maxium
+    pen.speed(4) # set the drawing speed to maxium
 
     # draw the sun rays using a loop
     pen.penup()
@@ -54,21 +53,21 @@ def draw_white_cloud():
     turtle.end_fill()
 
     turtle.penup()
-    turtle.goto(120, 220)
+    turtle.goto(120, 240)
     turtle.pendown()
     turtle.begin_fill()
     turtle.circle(50)
     turtle.end_fill()
 
     turtle.penup()
-    turtle.goto(270, 215)
+    turtle.goto(50, 215)
     turtle.pendown()
     turtle.begin_fill()
     turtle.circle(50)
     turtle.end_fill()
 
     turtle.penup()
-    turtle.goto(130, 155)
+    turtle.goto(130, 165)
     turtle.pendown()
     turtle.begin_fill()
     turtle.circle(50)
@@ -88,11 +87,30 @@ def draw_white_cloud():
     turtle.circle(50)
     turtle.end_fill()
 
+    turtle.penup()
+    turtle.goto(70, 140)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.circle(50)
+    turtle.end_fill()
+
+    turtle.penup()
+    turtle.goto(20, 170)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.circle(50)
+    turtle.end_fill()
+
+#rainy background
+def rainy_background():
+    turtle.bgcolor("light grey")
+    turtle.speed(4)
+
 #draw rainy cloud
 def draw_rain_cloud():
     turtle.penup()
     turtle.goto(250, 250)
-    turtle.speed(0)
+    turtle.speed(4)
     turtle.color("grey")
     turtle.begin_fill()
     turtle.pendown()
@@ -118,21 +136,151 @@ def draw_rain_cloud():
     turtle.forward(260)
     turtle.end_fill()
     turtle.hideturtle()
-"""
+
 def draw_rain():
     # set up rain
-    turtle.penup()
-    turtle.goto(250, 250)
-    turtle.pencolor("dark blue")
+    turtle.goto(300,250)
     turtle.right(90)
+    turtle.pencolor("dark blue")
     turtle.pendown()
     for i in range(10):
         turtle.forward(15)
         turtle.penup()
         turtle.forward(5)
         turtle.pendown()
+    turtle.penup()
+    turtle.goto(280, 250)
+    turtle.pendown()
+    for i in range(10):
+        turtle.forward(15)
         turtle.penup()
-"""""
+        turtle.forward(5)
+        turtle.pendown()
+
+    turtle.penup()
+    turtle.goto(260,250)
+    turtle.pendown()
+    for i in range(10):
+        turtle.forward(15)
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+    
+    turtle.penup()
+    turtle.goto(240, 250)
+    turtle.pendown()
+    for i in range(10):
+        turtle.forward(15)
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+
+    turtle.penup()
+    turtle.goto(220,250)
+    turtle.pendown()
+    for i in range(10):
+        turtle.forward(15)
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+    
+    turtle.penup()
+    turtle.goto(200, 250)
+    turtle.pendown()
+    for i in range(10):
+        turtle.forward(15)
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+
+    turtle.penup()
+
+    turtle.goto(200,250)
+    turtle.pendown()
+    for i in range(10):
+        turtle.forward(15)
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+    
+    turtle.penup()
+    turtle.goto(180, 250)
+    turtle.pendown()
+    for i in range(10):
+        turtle.forward(15)
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+
+    turtle.penup()
+    turtle.goto(160, 250)
+    turtle.pendown()
+    for i in range(10):
+        turtle.forward(15)
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+
+    turtle.penup()
+    turtle.goto(140, 250)
+    turtle.pendown()
+    turtle.penup()
+
+    for i in range(10):
+        turtle.forward(15)
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+
+    turtle.penup()
+    turtle.goto(120, 250)
+    turtle.pendown()
+    for i in range(10):
+        turtle.forward(15)
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+
+    turtle.penup()
+    turtle.goto(100, 250)
+    turtle.pendown()
+    for i in range(10):
+        turtle.forward(15)
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+
+    turtle.penup()
+    turtle.goto(80, 250)
+    turtle.pendown()
+    for i in range(10):
+        turtle.forward(15)
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+
+    turtle.penup()
+    turtle.goto(60, 250)
+    turtle.pendown()
+    for i in range(10):
+        turtle.forward(15)
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+
+    turtle.penup()
+    turtle.goto(40, 250)
+    turtle.pendown()
+    for i in range(10):
+        turtle.forward(15)
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+
+    turtle.penup()
+    turtle.left(90)
+    turtle.hideturtle()
+
 
 #Create function for grass
 def draw_grass():
@@ -354,23 +502,39 @@ def draw_white_chimney_trimming():
         pen.forward(80)
         pen.right(90)
     pen.hideturtle()
- 
+
+def thank_you():
+    turtle.penup()
+    turtle.goto(0,0)
+    turtle.hideturtle()
+    turtle.clear()
+    turtle.write("Group project by Viswa, Adam and Monique", align="center", font=("Arial", 16, "bold"))
+    #pause for 2 seconds
+    time.sleep(2)
+    #clear screen
+    turtle.clear()
+    turtle.write("Thank you for your time.", align="center", font=("Arial", 16, "bold"))
+    #pause for 2 seconds
+    time.sleep(2)
+    #quit program
+    turtle.bye()
 
 # main funciton to call the functions
 def main():
     # draw sky
     environment()
-    # draw backgroung
-    draw_background()
     #screen window
     window = turtle.Screen()
     #ask user for weather
     cloud_color = window.textinput("Will it rain?", "Yes or no")
     if cloud_color == "no":
+        draw_sunny_background()
         draw_white_cloud()
     else:
+        rainy_background()
         draw_rain_cloud()
-        #draw_rain()
+        draw_rain()
+    #draw grass
     draw_grass()
     #draw house
     draw_red_house()
@@ -413,7 +577,8 @@ def main():
     else:
         draw_white_chimney_trimming()    
     #end if
-    
+    #thank you message
+    thank_you()
  
 
 if __name__ == "__main__":
